1db931292a8b60f580e42d8bd4f035616cd31a8e
Jordan Winfery <jwinf843@gmail.com>
2019-09-04 16:42:31 +0000

    Initialized the project with general outline.

---
+++ .ipynb_checkpoints/calculator-checkpoint.py
@@ -0,0 +1,22 @@
+
+def add(x, y):
+    pass
+
+def subtract(x, y):
+    pass
+
+def divide(x, y):
+    # check if y is 0, there should be a test for that
+    pass
+    
+def multiply(x, y):
+    pass
+    
+def pow(x, y):
+    pass
+    
+def square(x):  # x*x, or pow(x, 2)
+    pass
+    
+def sqrt(x):  # x*1/2, or pow(x, 1/2)
+    pass
